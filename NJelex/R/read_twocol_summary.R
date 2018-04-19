760543fce2c4cff791878742d879d0b14e5728db
Alyssa Savo <as5797a@student.american.edu>
2018-04-19 17:01:15 -0400

    Expanding twocol_summary function

--- NJelex/R/read_twocol_summary.R
+++ NJelex/R/read_twocol_summary.R
@@ -14,15 +14,25 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 read_twocol_summary <- function(url) {
+  `%>%` <- magrittr::`%>%`
+  df <- list()
+
   pages <- tabulizer::extract_tables(url)
   for (i in 1:length(pages)) {
     # Drop whitespace columns that do not contain candidates or vote #s
-    columns <- apply(summary[[i]],2, function(x) any(grepl('Total|%',x)))
+    columns <- apply(pages[[i]],2, function(x) any(grepl('Total|%',x)))
     pages[[i]] <- pages[[i]][,columns]
     # This .pdf format has two columns containing tables of returns;
     # we stack the two columns on top of each other so we have one column
     # for candidates and one for votes
-    stacked <- dataframe(Candidate = c(pages[[i]][,1],pages[[i]][,3]),
+    df[[i]] <- data.frame(Candidate = c(pages[[i]][,1],pages[[i]][,3]),
                          Votes = c(pages[[i]][,2],pages[[i]][,4]))
+    # Convert empty rows to NAs and drop them
+    df[[i]] <- df[[i]] %>%
+      replace(. == '', NA) %>%
+      na.omit() %>%
+      # Split raw vote totals and percentages into two columns:
+      tidyr::separate(Votes, c("Raw Total","Percent"), " ")
   }
+  df
 }
